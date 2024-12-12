af50fb0e367435cad0fe0183ff39eaf6a0e31536
Iaroslava Krysova <yaroslavakrysova@Iaroslavas-MacBook-Pro.local>
2024-12-12 13:55:54 -0500

    decided to reorder classes(note: put all classes in different file in the end), made Toy class

--- program.py
+++ program.py
@@ -1,15 +1,24 @@
 import json
 
 
-class Program:
-    def __init__(self):
-        self.letters = []
+class Toy:
+    def __init__(self, name, category, description):
+        self._name = name
+        self._category = category
+        self._description = description
+
+    def turnIntoDict(self):     #this method returns info as dictionary
+        return {
+            "Name": self._name,
+            "Category": self._category,
+            "Description": self._description
+        }
 
 class Letter:
     pass
 
 
-class Toy:
+class Program:
     pass
 
 
